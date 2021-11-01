738e143668af866f65f5a3f76ec018222650ace5
= <=>
2021-11-01 16:23:49 -0300

    damage relations second type

--- src/controllers/pokemon.js
+++ src/controllers/pokemon.js
@@ -17,15 +17,58 @@ route.get("/pokemon/:param", async (req, res) => {
   try {
     const param = req.params.param;
     if (Number(param)) {
-      const pokemon = await Pokemon.findOne({ id: param });
+      const pokemon = await Pokemon.findOne({ name: param });
       const evolutionChain = await Pokemon.find({
         evolutionChain: pokemon.evolutionChain,
       });
 
-      const types = await Type.findOne({ name: pokemon.firstType });
+      const typesFirst = await Type.findOne({ name: pokemon.firstType });
+
+      if (pokemon.secondType) {
+        const typesSecond = await Type.findOne({ name: pokemon.secondType });
+
+        const typesAll = {
+          double_damage_from: typesFirst.double_damage_from.concat(
+            typesSecond.double_damage_from
+          ),
+          double_damage_to: typesFirst.double_damage_to.concat(
+            typesSecond.double_damage_to
+          ),
+          half_damage_from: typesFirst.half_damage_from.concat(
+            typesSecond.half_damage_from
+          ),
+          half_damage_to: typesFirst.half_damage_to.concat(
+            typesSecond.half_damage_to
+          ),
+        };
+
+        // função para remover types repetidos
+        function removeTypesRepeated(typesArray) {
+          const newTypesArray = (typesArray = typesArray.map(
+            (type) => type.name
+          ));
+
+          return [...new Set(newTypesArray)];
+        }
+
+        // Removendo types repetidos
+        typesAll.half_damage_to = removeTypesRepeated(typesAll.half_damage_to);
+        typesAll.half_damage_from = removeTypesRepeated(
+          typesAll.half_damage_from
+        );
+        typesAll.double_damage_from = removeTypesRepeated(
+          typesAll.double_damage_from
+        );
+        typesAll.double_damage_to = removeTypesRepeated(
+          typesAll.double_damage_to
+        );
+
+        pokemon.evolutionChain = evolutionChain;
+        return res.json({ pokemon, damageRelations: typesAll });
+      }
 
       pokemon.evolutionChain = evolutionChain;
-      return res.json({ pokemon, damageRelations: types });
+      return res.json({ pokemon, damageRelations: typesFirst });
     } else {
       const pokemon = await Pokemon.findOne({ name: param });
       const evolutionChain = await Pokemon.find({
